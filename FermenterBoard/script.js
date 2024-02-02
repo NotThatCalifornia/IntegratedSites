c0d9dd0d193f2a5a95fa27806f092f73c9885686
orafaj1_ford <orafaj1@ford.com>
2024-02-02 12:40:31 +0000

    wip

--- FermenterBoard/script.js
+++ FermenterBoard/script.js
@@ -28,19 +28,19 @@ function buildPage() {
       const headerContent = `
         <header>
             <img src="http://localhost/img/logo.png" alt="Not That California Brewing Company" />
-            <h1>${temperature}˚C</h1>
+            <h1><span id="temperature">${temperature}<span>˚C</h1>
         </header>
       `;
   
       // Build the info content
       const infoContent = Object.entries(info).map(([key, value]) => {
         if (key == "localUrl" || key == "ip") {
-            return `<p class="key">
-                <strong>${keyTranslations[key]}:</strong></p><p class="value">${value}
+            return `<p class="key"><strong>${keyTranslations[key]}:</strong></p>
+            <p class="value">${value}
                 <a href="http://${value}" class="btn btn-success">Go</a>
             </p>`;
         } else {
-            return `<p class="key"><strong>${keyTranslations[key]}:</strong></p><p class="value">${value}</p>`;
+            return `<p class="key"><strong>${keyTranslations[key]}:</strong></p><p class="value" id="key-${key}">${value}</p>`;
         }
       }).join('');
   
@@ -54,10 +54,10 @@ function buildPage() {
         <div id="content">
             ${headerContent}
             <div class="section modules values">
-                <p><span class="indicator ${values.relay1 ? 'enabled' : 'disabled'}"><i>${values.relay1 ? 'Enabled' : 'Disabled'}</i></span><span class="name">${keyTranslations["relay1"]}</span></p>
-                <p><span class="indicator ${values.relay2 ? 'enabled' : 'disabled'}"><i>${values.relay2 ? 'Enabled' : 'Disabled'}</i></span><span class="name">${keyTranslations["relay2"]}</span></p>
+                <p><span id="relay1" class="indicator ${values.relay1 ? 'enabled' : 'disabled'}"><i>${values.relay1 ? 'Enabled' : 'Disabled'}</i></span><span class="name">${keyTranslations["relay1"]}</span></p>
+                <p><span id="relay2" class="indicator ${values.relay2 ? 'enabled' : 'disabled'}"><i>${values.relay2 ? 'Enabled' : 'Disabled'}</i></span><span class="name">${keyTranslations["relay2"]}</span></p>
             </div>
-            <div class="section infor">
+            <div class="section info">
                 <div class="text-center">
                     <h4 id="info-header" class="btn btn-warning dropdown-toggle">
                         System info
@@ -65,7 +65,7 @@ function buildPage() {
                     </h4>
                 </div>
                 <div id="info" class="card">
-                    <p class="key"><strong>Device temperature:</strong></p><p class="value">${internalTemperature}˚C</p>
+                    <p class="key"><strong>Device temperature:</strong></p><p class="value" id="internalTemperature">${internalTemperature}˚C</p>
                     ${infoContent}
                 </div
             </div>
@@ -79,6 +79,17 @@ function buildPage() {
                 <div id="modules" class="card">
                     ${modulesContent}
                 </div>
+            </div>
+            <div class="footer section text-center">
+                <p>Last update: <span id="lastUpdate"></span></p>
+                <p>
+                    &copy; 
+                    <a href="https://notthatcalifornia.com">
+                        Not That California Brewing Co.
+                    </a>
+                    &
+                    Krafaj R&D Ltd
+                </p>
             </div>
         </div>
       `;
@@ -94,14 +105,68 @@ function buildPage() {
         $("#modules").toggle("fast");
     });
     $("#modules").hide();
+    setLastUpdated();
 
     }).catch(error => {
       console.error('Error fetching data:', error);
       // Handle the error, e.g., display an error message
       document.body.innerHTML = `<p style="color:red;">Error loading data. Please try again later.</p>`;
     });
-  }
-  
-  // Call the buildPage function once the DOM is fully loaded
-  document.addEventListener('DOMContentLoaded', buildPage);
+}
+
+function handleRelay(elementId, value) {
+    $(elementId).removeClass(value ? 'disabled' : 'enabled').addClass(value ? 'enabled' : 'disabled');
+}
+
+function fetchAndUpdateValues() {
+    fetch('/values')
+        .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+        })
+        .then(data => {
+        const temperatureElement = document.getElementById('temperature');
+        if (temperatureElement) {
+            temperatureElement.textContent = `${data.temp1}˚C`;
+        }
+        const internalTemperatureElement = document.getElementById('internalTemperature');
+        if (internalTemperatureElement) {
+            internalTemperatureElement.textContent = `${data.int}˚C`;
+        }
+
+        handleRelay('#relay1', data.relay1);
+        handleRelay('#relay2', data.relay2);
+
+        document.title = `Brewery name - ${data.temperature}˚C`;
+        setLastUpdated();
+        })
+        .catch(error => {
+        console.error('Error fetching /values:', error);
+        const errorDiv = document.getElementById('error');
+        if (errorDiv) {
+            errorDiv.textContent = 'Error loading data. Please try again later.';
+        }
+    });
+}
+
+function getDefaultFormattedDateTime() {
+    const now = new Date(Date.now());
+    const formattedDateTime = now.toLocaleString();
+    return formattedDateTime;
+}
+
+function setLastUpdated() {
+    const internalTemperatureElement = document.getElementById('lastUpdate');
+    if (internalTemperatureElement) {
+        internalTemperatureElement.textContent = getDefaultFormattedDateTime();
+    }
+}
+
+// Call fetchAndUpdateValues every 5 seconds
+setInterval(fetchAndUpdateValues, 5000);
+
+// Call the buildPage function once the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', buildPage);
   
